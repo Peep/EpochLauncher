ba1bfaf67630e3c52e8de2a601bf79fd3ffde79c
Michael Harris <harbingtarbl@gmail.com>
2014-10-25 01:38:22 -0400

    Started squirrel integration

--- EpochLauncher/App.xaml.cs
+++ EpochLauncher/App.xaml.cs
@@ -6,6 +6,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
+using Squirrel;
+
+
+
 namespace EpochLauncher
 {
     /// <summary>
@@ -13,5 +17,48 @@ namespace EpochLauncher
     /// </summary>
     public partial class App : Application
     {
-    }
+		static bool ShowWelcome = false;
+
+
+		static void Application_OnInitalInstall(Version x)
+		{
+			Console.WriteLine(x.Build);
+		}
+
+		static void Application_OnUpdate(Version x)
+		{
+			Console.WriteLine(x.Build);
+		}
+
+		static void Application_OnUninstall(Version x)
+		{
+			Console.WriteLine(x.Build);
+		}
+
+		static void Application_OnFirstRun()
+		{
+			Console.WriteLine("First Run");
+		}
+
+		static void Application_OnObsolete(Version x)
+		{
+			Console.WriteLine(x.Build);
+		}
+
+
+		private async void Application_Startup(object sender, StartupEventArgs e)
+		{
+			using (var manager = new UpdateManager(@"Releases\", "EpochLauncher", FrameworkVersion.Net45))
+			{
+				SquirrelAwareApp.HandleEvents(
+					onInitialInstall: new Action<Version>(Application_OnInitalInstall),
+					onAppUpdate: new Action<Version>(Application_OnUpdate),
+					onAppObsoleted: new Action<Version>(Application_OnObsolete),
+					onFirstRun: new Action(Application_OnFirstRun),
+					onAppUninstall: new Action<Version>(Application_OnUninstall));
+		
+				await manager.UpdateApp();
+			}
+		}
+	}
 }
