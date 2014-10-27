90b8cb053c445e91f9663fc6ad7a4740ac6c9f1a
Peep <peep@bmrf.me>
2014-10-27 15:25:44 -0300

    added version checking

--- EpochLauncher/App.xaml.cs
+++ EpochLauncher/App.xaml.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Windows;
-
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Squirrel;
 
 
@@ -19,38 +21,14 @@ namespace EpochLauncher
     {
 		static bool ShowWelcome = false;
 
-
-		static void Application_OnInitalInstall(Version x)
-		{
-			Console.WriteLine(x.Build);
-		}
-
-		static void Application_OnUpdate(Version x)
-		{
-			Console.WriteLine(x.Build);
-		}
-
-		static void Application_OnUninstall(Version x)
-		{
-			Console.WriteLine(x.Build);
-		}
-
-		static void Application_OnFirstRun()
-		{
-			Console.WriteLine("First Run");
-		}
-
-		static void Application_OnObsolete(Version x)
-		{
-			Console.WriteLine(x.Build);
-		}
-
+        public Version Version = new Version("0.1.1");
 
 		private async void Application_Startup(object sender, StartupEventArgs e)
 		{
-
-
-
+		    var uri = new Uri("https://launcher.bmrf.me/version.json");
+		    var wc = new WebClient();
+            wc.DownloadStringCompleted += async (s, args) => await Task.Run(() => CheckForUpdate(args.Result));
+		    wc.DownloadStringAsync(uri);
 #if !DEBUG
 			using (var manager = new UpdateManager(@"http://dev.bmrf.me/launcher/", "EpochLauncher", FrameworkVersion.Net45))
 			{
@@ -66,5 +44,15 @@ namespace EpochLauncher
 			}
 #endif
 		}
+
+        private void CheckForUpdate(string json)
+        {
+            var latestVersion = JsonConvert.DeserializeObject<Version>(json);
+            if (latestVersion > Version)
+            {
+                MessageBox.Show("Out of date!");
+                // update the app   
+            }
+        }
 	}
 }
