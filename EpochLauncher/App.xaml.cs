a75928c9d2cfec04b45d5e65e9a48206b4c84be7
Peep <peep@bmrf.me>
2014-10-27 15:29:11 -0300

    remove squirrel

--- EpochLauncher/App.xaml.cs
+++ EpochLauncher/App.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using System.Windows;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using Squirrel;
-
-
 
 namespace EpochLauncher
 {
@@ -19,30 +11,14 @@ namespace EpochLauncher
     /// </summary>
     public partial class App : Application
     {
-		static bool ShowWelcome = false;
-
         public Version Version = new Version("0.1.1");
 
-		private async void Application_Startup(object sender, StartupEventArgs e)
+		private void Application_Startup(object sender, StartupEventArgs e)
 		{
 		    var uri = new Uri("https://launcher.bmrf.me/version.json");
 		    var wc = new WebClient();
             wc.DownloadStringCompleted += async (s, args) => await Task.Run(() => CheckForUpdate(args.Result));
 		    wc.DownloadStringAsync(uri);
-#if !DEBUG
-			using (var manager = new UpdateManager(@"http://dev.bmrf.me/launcher/", "EpochLauncher", FrameworkVersion.Net45))
-			{
-				var info = await manager.CheckForUpdate();
-				if (info.FutureReleaseEntry == null)
-				{
-					MessageBox.Show(info.CurrentlyInstalledVersion.EntryAsString);
-					return;
-				}
-
-				await manager.DownloadReleases(info.ReleasesToApply);
-				await manager.ApplyReleases(info);
-			}
-#endif
 		}
 
         private void CheckForUpdate(string json)
