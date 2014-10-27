6a8a6c2aac5a93e5f74706cfc4072fa871872f70
Peep <peep@bmrf.me>
2014-10-27 20:08:16 -0300

    Started updater

---
+++ EpochLauncher/UpdatePromptWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+
+
+namespace EpochLauncher
+{
+    public partial class UpdatePromptWindow : Window
+    {
+        public UpdatePromptWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Updater.DoUpdate();
+            Close();
+            Application.Current.Dispatcher.BeginInvokeShutdown(DispatcherPriority.Send);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+
+    }
+}
---
+++ EpochLauncher/Updater.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using Newtonsoft.Json;
+
+namespace EpochLauncher
+{
+    public class Updater
+    {
+        public static void CheckForUpdates()
+        {
+            var uri = new Uri("https://launcher.bmrf.me/version.json");
+            var wc = new WebClient();
+            wc.DownloadStringCompleted += async (s, args) =>
+                    await Task.Run(() => OnDownloadCompleted(args.Result));
+            wc.DownloadStringAsync(uri);
+        }
+
+        private static void OnDownloadCompleted(string json)
+        {
+            var latestVersion = JsonConvert.DeserializeObject<Version>(json);
+            if (latestVersion < App.Version)
+                ShowUpdatePrompt();
+        }
+
+        private static void ShowUpdatePrompt()
+        {
+            var thread = new Thread(t =>
+            {
+                var window = new UpdatePromptWindow();
+                window.Show();
+                window.Closed += (s, e) => System.Windows.Threading.Dispatcher.ExitAllFrames();
+                System.Windows.Threading.Dispatcher.Run();
+            });
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+        }
+
+        public static void DoUpdate()
+        {
+            Process.Start("notepad.exe");
+        }
+    }
+}
