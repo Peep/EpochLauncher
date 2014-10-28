25ec2929535b288f89f3354411d6c2600d612459
Michael Harris <harbingtarbl@gmail.com>
2014-10-28 04:01:37 -0400

    Stuff been done hard

--- EpochLauncher/MainWindow.xaml.cs
+++ EpochLauncher/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,9 +26,52 @@ namespace EpochLauncher
 
 	    public class BoundMessager
 	    {
+		    private MainWindow _window;
+
+		    public delegate void LauncherWindowEvent();
+
+		    public event LauncherWindowEvent CloseEvent;
+		    public event LauncherWindowEvent MinimizeEvent;
+		    public event LauncherWindowEvent MaximizeEvent;
+
+
+		    public BoundMessager(MainWindow window)
+		    {
+			    _window = window;
+
+		    }
+
+
 		    public void HandleMessage(string type, string json)
 		    {
 			    MessageBox.Show(type, json);
+		    }
+
+		    public void Minimize()
+		    {
+				var temp = MinimizeEvent;
+			    if (temp != null)
+			    {
+					new Task(() => temp()).Start();
+			    }
+		    }
+
+			public void Close()
+			{
+				var temp = CloseEvent;
+				if (temp != null)
+				{
+					new Task(() => temp()).Start();
+				}
+			}
+
+		    public void Maximize()
+		    {
+			    var temp = MaximizeEvent;
+			    if (temp != null)
+			    {
+					new Task(() => temp()).Start();
+			    }
 		    }
 	    }
 
@@ -38,21 +82,69 @@ namespace EpochLauncher
         {
             InitializeComponent();
 
-			var settings = new CefSettings {PackLoadingDisabled = true};
+			var settings = new CefSettings
+			{
+				PackLoadingDisabled = false,
+				
+
+			};
 
 	        if (!Cef.Initialize(settings)) return;
 
 
-			WebView = new ChromiumWebBrowser();
+	        WebView = new ChromiumWebBrowser
+	        {
+		        BrowserSettings = new BrowserSettings
+		        {
+			        FileAccessFromFileUrlsAllowed = true,
+					WebGlDisabled = true,
+					JavaDisabled = true,	
+		        },
+
+	        };
+
+	        WebView.Loaded += delegate
+	        {
+				WebView.PreviewMouseDown += delegate
+		        {
+					DragMove();
+				};
+	        };
+
 			Browser.Children.Add(WebView);
-			WebView.Address = "test.html";
-			Messager = new BoundMessager();
+			WebView.Address = "cdn.bmrf.me/responsive.html";
+	        Messager = new BoundMessager(this);
+			Messager.CloseEvent += MessagerOnCloseEvent;
+			Messager.MinimizeEvent += MessagerMinimizeEvent;
+			Messager.MaximizeEvent += MessagerOnMaximizeEvent;
         }
+
+
+	    private void MessagerOnMaximizeEvent()
+	    {
+			Dispatcher.Invoke(() =>
+			{
+				WindowState = WindowState.Maximized;
+			});
+	    }
+
+	    private void MessagerOnCloseEvent()
+	    {
+		    Dispatcher.Invoke(Close);
+	    }
+
+	    private void MessagerMinimizeEvent()
+	    {
+		    Dispatcher.Invoke(() =>
+		    {
+			    WindowState = WindowState.Minimized;
+		    });
+	    }
 
 	    private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
 	    {
-			WebView.RegisterJsObject("message", Messager);
-		 
+		    WebView.RegisterJsObject("launcher", Messager);
+		    WebView.ShowDevTools();
 	    }
     }
 }
