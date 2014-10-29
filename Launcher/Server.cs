3415dc7ed66063852863019726bbeecd3533b5d9
Peep <peep@bmrf.me>
2014-10-29 12:27:33 -0300

    Update server view

--- Launcher/Server.cs
+++ Launcher/Server.cs
@@ -1,17 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Launcher
 {
-    public class Server
+    public class Server : INotifyPropertyChanged
     {
-        public string Hostname { get; internal set; }
-        public int CurrentPlayers { get; internal set; }
-        public int MaxPlayers { get; internal set; }
-        public int Ping { get; internal set; }
-        public bool IsOfficial { get; internal set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _hostname;
+        private int _currentPlayers;
+        private int _maxPlayers;
+        private int _ping;
+        private bool _isOfficial;
+
+        public string Hostname
+        {
+            get { return _hostname; }
+            internal set
+            {
+                _hostname = value;
+                OnPropertyChanged("Hostname");
+            }
+        }
+
+        public int CurrentPlayers
+        {
+            get { return _currentPlayers; }
+            internal set
+            {
+                _currentPlayers = value;
+                OnPropertyChanged("CurrentPlayers");
+            }
+        }
+
+        public int MaxPlayers
+        {
+            get { return _maxPlayers; }
+            internal set
+            {
+                _maxPlayers = value;
+                OnPropertyChanged("MaxPlayers");
+            }
+        }
+
+        public int Ping
+        {
+            get { return _ping; }
+            internal set
+            {
+                _ping = value;
+                OnPropertyChanged("Ping");
+            }
+        }
+
+        public bool IsOfficial
+        {
+            get { return _isOfficial; }
+            internal set
+            {
+                _isOfficial = value;
+                OnPropertyChanged("IsOfficial");
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
     }
 }
--- Launcher/ServerBrowser.cs
+++ Launcher/ServerBrowser.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SSQLib;
 
 namespace Launcher
 {
     public class ServerBrowser
     {
+        private SSQL _query;
+
+        public ServerBrowser()
+        {
+            //_query = new SSQL();
+        }
     }
 }
