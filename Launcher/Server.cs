906b71785e766dadf39c4a81c96265f47c82a6a1
Peep <peep@bmrf.me>
2014-10-28 22:06:34 -0300

    Add SSQLib, remake Library project

---
+++ Launcher/Server.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Launcher
+{
+    public class Server
+    {
+        public string Hostname { get; internal set; }
+        public int CurrentPlayers { get; internal set; }
+        public int MaxPlayers { get; internal set; }
+        public int Ping { get; internal set; }
+        public bool IsOfficial { get; internal set; }
+    }
+}
