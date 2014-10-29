73906e64a31fa23ad8855e05a3f415dea954745f
Peep <peep@bmrf.me>
2014-10-28 21:22:58 -0300

    Started on server browser

---
+++ Launcher/ServerBrowser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Launcher
+{
+    public class ServerBrowser
+    {
+    }
+
+    public class Server
+    {
+        public string Hostname { get; set; }
+        public int CurrentPlayers { get; set; }
+        public int MaxPlayers { get; set; }
+        public int? Ping { get; set; }
+    }
+}
