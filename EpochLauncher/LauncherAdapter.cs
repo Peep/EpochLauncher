1e3de316e0851b00ee662c05ab22bb71b7874f2b
Michael Harris <harbingtarbl@gmail.com>
2014-10-31 01:04:15 -0400

    Uhh MVVM HURD

---
+++ EpochLauncher/LauncherAdapter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpochLauncher
+{
+	public class LauncherAdapter
+	{
+		private readonly ILauncher
+
+
+		public string StartGame(int serverId)
+		{
+			
+		}
+	}
+}
---
+++ EpochLauncher/StartGameResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpochLauncher
+{
+	public class StartGameResult
+	{
+		public enum ResultType
+		{
+			Success,
+			Failure,
+			VersionOutOfDate,
+			ConnectionError,
+			UnknownServer,
+			pLeAse_uDdAtE_ThE_GaMe_BeCasE_PeEp_Is_GaY_AnD_He_HaTeS_YoU_DOT_JpEg
+		}
+
+		public ResultType Result;
+		public string Info;
+	}
+}
