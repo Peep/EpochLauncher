7ef9ee0cb33156ff35dda20e6197ad978d7c17f9
Peep <peep@bmrf.me>
2014-10-31 04:38:23 -0300

    Server browser progress

--- Launcher/ServerBrowser.cs
+++ Launcher/ServerBrowser.cs
@@ -28,6 +28,14 @@ namespace Launcher
             });
         }
 
+        public void Refresh(int serverHandle)
+        {
+            if (!Servers.ContainsKey(serverHandle)) return;
+
+            var address = Servers[serverHandle].Address.Split(':');
+            QueryServer(new IPEndPoint(IPAddress.Parse(address[0]), Convert.ToInt32(address[1])));
+        }
+
         void ReceiveServers(ReadOnlyCollection<IPEndPoint> endPoints)
         {
             foreach (var ip in endPoints.Where(ip => ip.Address.ToString() != "0.0.0.0"))
@@ -38,9 +46,18 @@ namespace Launcher
         {
             var server = ServerQuery.GetServerInstance(EngineType.Source, endPoint);
             var info = server.GetInfo();
+            var handle = info.Address.GetHashCode();
 
-            var hash = info.GetHashCode();
-            Servers.Add(hash, info);
+            if (Servers.ContainsKey(handle))
+            {
+                Servers[handle] = info;
+                // Call object modified event
+            }
+            else
+            {
+                Servers.Add(handle, info);
+                // Call object added event
+            }
         }
     }
 }
