a0d8260167c500a8e2d321d0ac4df9f783ce78f2
Corey Chase <peep@bmrf.me>
2014-11-02 21:51:05 -0400

    Retrieve servers from json

---
+++ Launcher/OfficialServerInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Launcher
+{
+    public class OfficialServerInfo
+    {
+        public string IP { get; private set; }
+        public int Port { get; private set; }
+
+        public OfficialServerInfo(string ip, int port)
+        {
+            IP = ip;
+            Port = port;
+        }
+
+        public IPEndPoint GetEndpoint()
+        {
+            return new IPEndPoint(IPAddress.Parse(IP), Port);
+        }
+    }
+}
--- Launcher/ServerBrowser.cs
+++ Launcher/ServerBrowser.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Net;
 using Launcher.Events;
+using Newtonsoft.Json;
 using QueryMaster;
 
 namespace Launcher
@@ -12,53 +13,40 @@ namespace Launcher
     public class ServerBrowser
     {
         public Dictionary<int, ServerInfo> Servers { get; internal set; }
+        public HashSet<OfficialServerInfo> OfficialServers { get; internal set; }
 
 	    public int ServerCount;
 
         public event EventHandler<ServerEventArgs> ServerAdded;
         public event EventHandler<ServerEventArgs> ServerChanged;
 
-        public static readonly HashSet<IPEndPoint> OfficialServers = new HashSet<IPEndPoint>
-			{
-				new IPEndPoint(IPAddress.Parse("128.165.214.23"), 2303),
-                new IPEndPoint(IPAddress.Parse("5.62.103.29"), 2303),
-                new IPEndPoint(IPAddress.Parse("88.198.221.81"), 2303),
-                new IPEndPoint(IPAddress.Parse("69.162.70.162"), 2303),
-                new IPEndPoint(IPAddress.Parse("69.162.70.162"), 2403),
-                new IPEndPoint(IPAddress.Parse("193.192.58.69"), 2303),
-                new IPEndPoint(IPAddress.Parse("193.192.58.69"), 2403),
-                new IPEndPoint(IPAddress.Parse("192.254.71.234"), 2303),
-                new IPEndPoint(IPAddress.Parse("192.254.71.234"), 2403),
-                new IPEndPoint(IPAddress.Parse("178.33.137.135"), 2313),
-                new IPEndPoint(IPAddress.Parse("192.95.30.52"), 2303),
-                new IPEndPoint(IPAddress.Parse("192.99.101.126"), 2303),
-                new IPEndPoint(IPAddress.Parse("188.165.250.119"), 2365),
-                new IPEndPoint(IPAddress.Parse("62.210.83.13"), 3003),
-                new IPEndPoint(IPAddress.Parse("188.165.233.104"), 2503),
-                new IPEndPoint(IPAddress.Parse("188.165.233.104"), 2603),
-                new IPEndPoint(IPAddress.Parse("192.254.71.233"), 3203),
-                new IPEndPoint(IPAddress.Parse("192.254.71.233"), 3303),
-                new IPEndPoint(IPAddress.Parse("103.13.103.37"), 2303),
-                new IPEndPoint(IPAddress.Parse("178.33.226.75"), 2323),
-                new IPEndPoint(IPAddress.Parse("69.162.93.250"), 2702),
-			};
-
-        public void Refresh()
+        public void Refresh(bool verifiedOnly = true)
         {
-	        ServerCount = 0;
+            if (OfficialServers == null)
+                OfficialServers = new HashSet<OfficialServerInfo>();
+
+            ServerCount = 0;
             if (Servers == null)
-				Servers = new Dictionary<int, ServerInfo>();
+                Servers = new Dictionary<int, ServerInfo>();
             else
                 Servers.Clear();
 
-            //var master = MasterQuery.GetMasterServerInstance(EngineType.Source);
-            //master.GetAddresses(Region.Rest_of_the_world, ReceiveServers, new IpFilter()
-            //{
-            //    IsDedicated = true, GameDirectory = "Arma3"
-            //});
+            if (verifiedOnly)
+                using (var wc = new WebClient())
+                    OfficialServers = JsonConvert.DeserializeObject<HashSet<OfficialServerInfo>>
+                        (wc.DownloadString("https://launcher.bmrf.me/servers.json"));
+            else
+            {
+                var master = MasterQuery.GetMasterServerInstance(EngineType.Source);
+                master.GetAddresses(Region.Rest_of_the_world, ReceiveServers, new IpFilter()
+                {
+                    IsDedicated = true,
+                    GameDirectory = "Arma3"
+                });
+            }
 
             foreach (var server in OfficialServers)
-                QueryServer(server);
+                QueryServer(server.GetEndpoint());
         }
 
         public void Refresh(int serverHandle)
